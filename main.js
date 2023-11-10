d4098f2a86c01b0bef4f5c8a9206b9fc71e54529
mazurluc <108153342+mazurluc@users.noreply.github.com>
2023-11-10 16:27:55 -0300

    kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk

---
+++ main.js
@@ -0,0 +1,32 @@
+unction tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+ 
+function TocaSomClap() {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_clap').onclick = TocaSomClap;
+function TocaSomTim(){
+    document.querySelector('#som_tecla_tim').play()
+    document.querySelector('.tecla_tim').onclick =TocaSomTim;
+    function TocaSomPuff(){
+        document.querySelector('#som_tecla_puff').play()
+        document.querySelector('Tecla_puff').onclick =TocaSomPuff;
+        function TocaSomSplash(){
+            document.querySelector('#som_tecla_splash').play()
+            document.querySelector('Tecla_splash').onclick =TocaSomPuff;
+            function TocaSomToim(){
+                document.querySelector('#som_tecla_toim').play()
+                document.querySelector('tecla_toim').onclick =TocaSomToim;
+                fuction TocaSomSplash(){
+                    document.querySelector('#som_tecla_psh').play()
+                    document.querySelector('tecla_psh').onclick =TocaSomPsh;
+                    fuction TocaSomTic(){
+                        document.querySelector('#som_tecla_tic').play()
+                        document.querySelector('tecla_tic').onclick =TocaSomTic;
+                        fuction TocaSomTom(){
+                            document.querySelector('#som_tecla_tom').play()
+                            document.querySelector('tecla_tom').onclick =TocaSomTom;
